6e8fcbe663cc00e37832202a54c234577bc55f45
Rick van den Bosch <rickvdbosch@outlook.com>
2026-03-26 16:26:14 +0100

    More constants!

--- Bosqora.Heyzine/Constants/Constants.cs
+++ Bosqora.Heyzine/Constants/Constants.cs
@@ -2,13 +2,43 @@ namespace Bosqora.Heyzine;
 
 internal static class Constants
 {
+    internal const string ACCESS_ADD_POSTFIX = "access-add";
+
+    internal const string ACCESS_REMOVE_POSTFIX = "access-remove";
+
+    internal const string ACCESS_SETUP_POSTFIX = "access-setup";
+
     internal const string API_REST_POSTFIX = "rest";
+
+    internal const string API_ASYNC_POSTFIX = "async";
 
     internal const string API_URL = "https://heyzine.com/api1/";
 
+    internal const string APIKEY_SETTINGNAME = "HeyzineApiKey";
+
+    internal const string BOOKSHELF_ADD_POSTFIX = "bookshelf-add";
+
+    internal const string BOOKSHELF_FLIPBOOKS_POSTFIX = "bookshelf-flipbooks";
+
+    internal const string BOOKSHELF_LIST_POSTFIX = "bookshelf-list";
+
+    internal const string BOOKSHELF_REMOVE_POSTFIX = "bookshelf-remove";
+
+    internal const string BOOKSHELF_SOCIAL_POSTFIX = "bookshelf-social";
+
     internal const string CLIENTID_SETTINGNAME = "HeyzineClientId";
 
+    internal const string FLIPBOOK_DELETE_POSTFIX = "flipbook-delete";
+
+    internal const string FLIPBOOK_DETAILS_POSTFIX = "flipbook-details";
+
+    internal const string FLIPBOOK_LIST_POSTFIX = "flipbook-list";
+
+    internal const string FLIPBOOK_SOCIAL_POSTFIX = "flipbook-social";
+
     internal const string HTTPCLIENT_NAME = "Bosqora.Heyzine.HttpClient";
+
+    internal const string OEMBED_POSTFIX = "oembed";
     
-    internal const string ERRORS_ENVVAR_NOTSET = "Environment variable {0} has not been configured correctly.";
+    internal const string ERRORS_ENVVAR_NOTSET = "Environment variable '{0}' has not been configured correctly.";
 }
