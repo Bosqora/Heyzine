f1dda88e902b5426c914d74f90764285cb9f4f66
Rick van den Bosch <rickvdbosch@outlook.com>
2026-03-26 16:24:37 +0100

    Additional methods on the interface and the RestClient

--- Bosqora.Heyzine/Clients/HeyzineRestClient.cs
+++ Bosqora.Heyzine/Clients/HeyzineRestClient.cs
@@ -31,12 +31,83 @@ public class HeyzineRestClient : IHeyzineRestClient
 
     #endregion
 
-    public async Task<HeyzineResponse?> ConvertPdfAsync(Uri pdfLocation)
+    public Task<HeyzineResponse?> ConvertPdfAsync(Uri pdfLocation, CancellationToken cancellationToken = default)
     {
-        HeyzineRequest request = new(pdfLocation, _clientId);
-        var response = await _httpClient.PostAsJsonAsync(Constants.API_REST_POSTFIX, request);
+        ArgumentNullException.ThrowIfNull(pdfLocation, nameof(pdfLocation));
+
+        return ConvertPdfAsync(new HeyzineConversionRequest
+        {
+            Pdf = pdfLocation,
+        }, cancellationToken);
+    }
+
+    public async Task<HeyzineResponse?> ConvertPdfAsync(HeyzineConversionRequest request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request, nameof(request));
+
+        var response = await _httpClient.PostAsJsonAsync(Constants.API_REST_POSTFIX, CreateAuthenticatedRequest(request), cancellationToken);
         response.EnsureSuccessStatusCode();
 
-        return await response.Content.ReadFromJsonAsync<HeyzineResponse?>();
+        return await response.Content.ReadFromJsonAsync<HeyzineResponse?>(cancellationToken: cancellationToken);
+    }
+
+    public async Task<HeyzineOEmbedResponse?> GetOEmbedAsync(Uri flipbookUrl, int? maxWidth = null, int? maxHeight = null, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(flipbookUrl, nameof(flipbookUrl));
+
+        var query = new List<string>
+        {
+            $"url={Uri.EscapeDataString(flipbookUrl.AbsoluteUri)}",
+            "format=json"
+        };
+
+        if (maxWidth is not null)
+        {
+            query.Add($"maxwidth={maxWidth.Value}");
+        }
+
+        if (maxHeight is not null)
+        {
+            query.Add($"maxheight={maxHeight.Value}");
+        }
+
+        var response = await _httpClient.GetAsync($"{Constants.OEMBED_POSTFIX}?{string.Join("&", query)}", cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<HeyzineOEmbedResponse?>(cancellationToken: cancellationToken);
+    }
+
+    public async Task<HeyzineResponse?> StartPdfConversionAsync(HeyzineConversionRequest request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request, nameof(request));
+
+        var response = await _httpClient.PostAsJsonAsync(Constants.API_ASYNC_POSTFIX, CreateAuthenticatedRequest(request), cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<HeyzineResponse?>(cancellationToken: cancellationToken);
+    }
+
+    private HeyzineConversionRequest CreateAuthenticatedRequest(HeyzineConversionRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request.Pdf, nameof(request.Pdf));
+
+        return new HeyzineConversionRequest
+        {
+            BackgroundColor = request.BackgroundColor,
+            ClientId = _clientId,
+            Description = request.Description,
+            Download = request.Download,
+            FullScreen = request.FullScreen,
+            Logo = request.Logo,
+            Pdf = request.Pdf,
+            PreviousNext = request.PreviousNext,
+            PrivateNote = request.PrivateNote,
+            Share = request.Share,
+            ShowInfo = request.ShowInfo,
+            Subtitle = request.Subtitle,
+            Tags = request.Tags,
+            Template = request.Template,
+            Title = request.Title,
+        };
     }
 }
--- Bosqora.Heyzine/Clients/Interfaces/IHeyzineRestClient.cs
+++ Bosqora.Heyzine/Clients/Interfaces/IHeyzineRestClient.cs
@@ -4,5 +4,11 @@ namespace Bosqora.Heyzine.Clients.Interfaces;
 
 public interface IHeyzineRestClient
 {
-    Task<HeyzineResponse?> ConvertPdfAsync(Uri pdfLocation);
+    Task<HeyzineResponse?> ConvertPdfAsync(Uri pdfLocation, CancellationToken cancellationToken = default);
+
+    Task<HeyzineResponse?> ConvertPdfAsync(HeyzineConversionRequest request, CancellationToken cancellationToken = default);
+
+    Task<HeyzineOEmbedResponse?> GetOEmbedAsync(Uri flipbookUrl, int? maxWidth = null, int? maxHeight = null, CancellationToken cancellationToken = default);
+
+    Task<HeyzineResponse?> StartPdfConversionAsync(HeyzineConversionRequest request, CancellationToken cancellationToken = default);
 }
