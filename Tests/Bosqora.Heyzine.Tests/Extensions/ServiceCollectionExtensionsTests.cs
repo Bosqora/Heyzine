9e5b5d7a514594f8432c9f3c1472b6221d91f043
Rick van den Bosch <rickvdbosch@outlook.com>
2026-03-26 12:30:42 +0100

    More unit tests!

---
+++ Tests/Bosqora.Heyzine.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.DependencyInjection;
+
+using Bosqora.Heyzine.Clients.Interfaces;
+using Bosqora.Heyzine.Extensions;
+
+namespace Bosqora.Heyzine.Tests.Extensions;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddHeyzine_ReturnsSameServiceCollection()
+    {
+        var services = new ServiceCollection();
+
+        var result = services.AddHeyzine();
+
+        Assert.Same(services, result);
+    }
+
+    [Fact]
+    public void AddHeyzine_RegistersHeyzineRestClientAsTransient()
+    {
+        var services = new ServiceCollection();
+
+        services.AddHeyzine();
+
+        var descriptor = Assert.Single(services, sd =>
+            sd.ServiceType == typeof(IHeyzineRestClient));
+        Assert.Equal(ServiceLifetime.Transient, descriptor.Lifetime);
+    }
+
+    [Fact]
+    public void AddHeyzine_RegistersNamedHttpClient()
+    {
+        var services = new ServiceCollection();
+
+        services.AddHeyzine();
+
+        Assert.Contains(services, sd =>
+            sd.ServiceType == typeof(IHttpClientFactory));
+    }
+
+    [Fact]
+    public void AddHeyzine_ConfiguresHttpClientBaseAddress()
+    {
+        var services = new ServiceCollection();
+        services.AddHeyzine();
+        using var provider = services.BuildServiceProvider();
+        var factory = provider.GetRequiredService<IHttpClientFactory>();
+
+        using var client = factory.CreateClient("Bosqora.Heyzine.HttpClient");
+
+        Assert.Equal(new Uri("https://heyzine.com/api1/"), client.BaseAddress);
+    }
+}
